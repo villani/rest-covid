68ba20e0db334f3d2e67512eeb9dfd07883770d1
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2020-10-14 19:19:25 -0300

    Configuraçoes iniciais do projeto.

---
+++ src/main/java/br/com/fiap/restcovid/RestCovidApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.restcovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/restcovid/RestCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.restcovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

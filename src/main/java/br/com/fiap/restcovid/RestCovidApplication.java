370d18424e3c225c1999ec9d92233dd6a9191748
fblopes147 <felipeblopes@gmail.com>
2020-10-26 21:44:04 -0300

    Configuração CORS

--- src/main/java/br/com/fiap/restcovid/RestCovidApplication.java
+++ src/main/java/br/com/fiap/restcovid/RestCovidApplication.java
@@ -2,6 +2,9 @@ package br.com.fiap.restcovid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,4 +16,15 @@ public class RestCovidApplication {
 		SpringApplication.run(RestCovidApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("*");
+			}
+		};
+	}
 }
